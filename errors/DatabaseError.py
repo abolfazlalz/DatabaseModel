2962e5f6a017f2f30c4b5fce70181f7010d506da
abolfazlalz <mrabolfazlalz@gmail.com>
2020-12-27 17:23:15 +0330

    Add HostController
    Add a host controller for manage databases in a host

---
+++ errors/DatabaseError.py
@@ -0,0 +1,16 @@
+class DatabaseError(Exception):
+    def __init__(self, *args: object):
+        self.msg = None
+
+    def __str__(self):
+        return self.msg
+
+
+class DatabaseNotExistsError(DatabaseError):
+    def __init__(self):
+        self.msg = "can't found database"
+
+
+class DatabaseBadNameError(DatabaseError):
+    def __init__(self):
+        self.msg = "Bad database name"
---
+++ utils/HostController.py
@@ -0,0 +1,48 @@
+import mysql.connector
+from errors.DatabaseError import DatabaseNotExistsError
+from errors.DatabaseError import DatabaseBadNameError
+import re
+
+
+class HostController:
+    def __init__(self, host, username, password):
+        self.db = mysql.connector.connect(host=host, user=username, password=password)
+
+    def create_database(self, database):
+        query = "CREATE DATABASE %s" % database
+        cursor = self.db.cursor()
+        cursor.execute(query)
+
+    def create_database_if_exists(self, database):
+        query = "CREATE DATABASE IF NOT EXISTS %s" % database
+        cursor = self.db.cursor()
+        cursor.execute(query)
+
+    def drop_database(self, database):
+        try:
+            cursor = self.db.cursor()
+            cursor.execute("DROP DATABASE %s" % database)
+        except mysql.connector.errors.DatabaseError as error:
+            if error.msg == 'Can\'t drop database \'abolfazl\'; database doesn\'t exist':
+                raise DatabaseNotExistsError()
+
+    def drop_database_if_exists(self, database):
+        cursor = self.db.cursor()
+        cursor.execute("DROP DATABASE IF EXISTS %s" % database)
+
+    def database_exists(self, database):
+        cursor = self.db.cursor()
+        cursor.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '%s'" % database)
+        result = []
+        for database in cursor:
+            result.append(database)
+        return result
+
+    def show_databases(self):
+        cursor = self.db.cursor()
+        cursor.execute("SHOW DATABASES")
+        result = []
+
+        for database in cursor:
+            result.append(database[0])
+        return result
